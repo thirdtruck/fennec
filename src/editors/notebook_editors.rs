e13a3f42e3af5da059d78eb25500c3ce86b12dc0
Jaycie Holder <jc@thirdtruck.org>
2022-12-14 09:50:03 -0500

    Resume serializing all properties for NotebookEditor and SnippetEditor

--- src/editors/notebook_editors.rs
+++ src/editors/notebook_editors.rs
@@ -5,9 +5,7 @@ use crate::prelude::*;
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct NotebookEditor {
     selected_notebook: Notebook,
-    #[serde(skip_serializing)]
     snippet_editor: Option<SnippetEditor>,
-    #[serde(skip_serializing)]
     selected_snippet_index: Option<usize>,
 }
 
