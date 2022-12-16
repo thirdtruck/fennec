a41f2d4945ac50b77f20ddfda1ae15379cea2682
Jaycie Holder <jc@thirdtruck.org>
2022-12-16 09:57:52 -0500

    Add preliminary CLI to the add-snippet binary

--- src/bin/add-snippet.rs
+++ src/bin/add-snippet.rs
@@ -1,21 +1,62 @@
+use clap::{Parser, Subcommand};
+
 use fennec::prelude::*;
 
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Args {
+    // Append the new snippet to the end of the notebook [default: prepend]
+    #[arg(short, long)]
+    append: bool,
+
+    // Include this description with the new snippet
+    #[arg(short, long)]
+    description: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug)]
+#[derive(Subcommand)]
+enum Commands {
+    Tunic,
+    English,
+}
+
 fn main() {
+    let args = Args::parse();
+
     println!("Loading notebook...");
     match notebook_from_yaml_file(DEFAULT_NOTEBOOK_FILE) {
         Ok((mut notebook, _yaml)) => {
-            println!("Adding snippet...");
+            println!("Prepending snippet...");
 
-            let english_word: Word = "SomeEnglish".into();
-            let tunic_word: Word = vec![0x99].into();
+            let starting_word = match &args.command {
+                Some(Commands::Tunic) => vec![0x99].into(),
+                Some(Commands::English) => "SomeEnglish".into(),
+                None => vec![0x99].into(),
+            };
+
+            let words: Vec<Word> = vec![starting_word].into();
+
+            let description = if let Some(description) = args.description {
+                description
+            } else {
+                "ADD_DESCRIPTION_HERE".into()
+            };
 
             let snippet = Snippet {
-                words: vec![english_word, tunic_word].into(),
+                words,
                 source: Some(Source::ManualPageNumber(0)),
-                description: "ADD_DESCRIPTION_HERE".into(),
+                description,
             };
                 
-            notebook.snippets.push(snippet);
+            if args.append {
+                notebook.snippets.push(snippet);
+            } else {
+                notebook.snippets.insert(0, snippet);
+            }
 
             println!("Saving notebook...");
 
