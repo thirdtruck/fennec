244d3095adbcd5ebb7a32978b84363cdea7186da
Jaycie Holder <jc@thirdtruck.org>
2023-01-22 11:22:19 -0500

    Reorder fields for easier editing

--- src/language/snippets.rs
+++ src/language/snippets.rs
@@ -4,11 +4,11 @@ use crate::prelude::*;
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Snippet {
-    pub words: Vec<Word>,
     pub source: Option<Source>,
     pub description: String,
-    pub notes: Vec<Note>,
     pub transcribed: bool,
+    pub notes: Vec<Note>,
+    pub words: Vec<Word>,
 }
 
 impl Snippet {
